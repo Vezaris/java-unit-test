83d75edfbf71aeb6949d8f3ea64edf6f2546d2a1
Sergey Belenko <belenkoserg@yandex.ru>
2023-08-04 07:50:29 +0100

    My unit tests

---
+++ task_3.java
@@ -0,0 +1,39 @@
+// Напиши аннотацию для параметризованных тестов
+@RunWith(Parameterized.class) 
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	// Инициализируй поля класса в конструкторе
+    this.age = age;
+    this.result = result;   
+  }
+
+  // Пометь метод аннотацией для параметров
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+	return new Object[][] {
+        // Заполни массив тестовыми данными и ожидаемым результатом
+        {17, false},
+        {18, true},
+        {19, true}, 
+        {21, true}, 
+    };
+  }
+
+  @Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+    boolean defolt;  
+    // Передай сюда возраст пользователя 
+    boolean isAdult = program.checkIsAdult(age);
+    if (age >= 18) {
+        defolt = true;
+    }
+    else defolt = false;  
+	// Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+    assertEquals("Возраст меньше 18 " + age, defolt, isAdult);
+	}
+}
